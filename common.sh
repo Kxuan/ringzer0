36bd78f6fb9b82a21e7a743cd7d7c5c1648c443c
kXuan <kxuanobj@gmail.com>
2017-03-29 16:39:05 +0800

    retrive_code_block

--- common.sh
+++ common.sh
@@ -30,5 +30,31 @@ func_curl() {
     curl "${args[@]}" -s -A "RingZer0/0.1" -H 'Cookie: PHPSESSID='$PHPSESSID "$@"
 }
 
+retrive_challenge() {
+    local rc
+    pushd $(dirname ${BASH_SOURCE[0]})/lib/ >/dev/null
+    curl 'https://ringzer0team.com/challenges/'$QID -H 'Referer: https://ringzer0team.com/challenges' | node parser.js | dos2unix
+    rc=$?
+    popd >/dev/null
+    return $rc
+}
+
+retrive_code_block() {
+    local keyword="$1"
+    local write_code=false
+    while read -r line; do
+        if [[ $line == "----- BEGIN "$keyword" -----" ]]; then
+            write_code=true
+            continue
+        elif [[ $line == "----- END "$keyword" -----" ]]; then
+            return
+        fi
+
+        if $write_code; then
+            echo "$line"
+        fi
+    done
+}
+
 override curl func_curl
 QID=$(basename $(dirname $(readlink -f ${BASH_SOURCE[1]})))
